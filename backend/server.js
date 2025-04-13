dc367641bde12c9c0e53af7c49080c7d43b513c0
Shivanggupta12 <f20220643@pilani.bits-pilani.ac.in>
2025-04-13 23:57:47 +0530

    Add CORS middleware and update server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.routes.js"
 
-dotenv.config();
-const PORT =process.env.PORT || 5000
 const app = express();
 
+// Use cors middleware
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
-app.use("/api/products",productRoutes)
+app.use("/api/products", productRoutes);
+
 app.listen(PORT, () => {
   connectDB();
-  console.log("Server started on http://localhost:"+ PORT);
+  console.log("Server started on http://localhost:" + PORT);
 });
